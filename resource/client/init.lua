efc7ac3842c327f92a2032a631dfd715cce94caf
Sergio <89666307+feelfreetofee@users.noreply.github.com>
2025-12-19 21:40:44 +0100

    Add menu requirement to init.lua

--- resource/client/init.lua
+++ resource/client/init.lua
@@ -1,6 +1,6 @@
 require('@rs_lib/resource/client/nui')
 require('@rs_lib/resource/client/nui/clipboard')
+require('@rs_lib/resource/client/nui/menu')
 
 require('@rs_lib/resource/client/cache')
 require('@rs_lib/resource/client/gizmo')
-
